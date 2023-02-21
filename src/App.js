0a956a315e1fdc6bd9ec4cd6dd568006b00f16f5
GAYATHRI-3200 <subbusubramani1995@gmail.com>
2023-02-21 10:42:22 +0000

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from './components/Counter'
+
+import Welcome from './components/Welcome'
+
+const App = () => {
+  return (
+    <div>
+      <Welcome />
+      <Counter />
+    </div>
+  )
+}
+export default App
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,5 @@
+import './index.css'
+
+const Welcome = () => <h1 className="heading">Hello, User</h1>
+
+export default Welcome
